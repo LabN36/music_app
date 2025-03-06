a458c96ba37d6fd74f550d51382318e5d76b1532
Rishabh Agrawal <rishabh.agrawal@applyboard.com>
2025-03-06 23:09:41 +0530

    feat: Implement local storage for songs with retrieval and saving methods

--- lib/data/song/local_storage_data.dart
+++ lib/data/song/local_storage_data.dart
@@ -1,9 +1,27 @@
+import 'dart:convert';
+
+import 'package:music_app/db_helper/db_helper.dart';
+
 import 'song_data_source.dart';
 import 'song_model.dart';
 
 class SongLocalStorageData implements SongDataSource {
+  AbstractDBHelder dbHelper;
+  SongLocalStorageData(this.dbHelper);
   @override
-  Future<List<SongModel>?> getSongs() {
-    throw UnimplementedError();
+  Future<List<SongModel>?> getSongs() async {
+    final songsString = await dbHelper.getData('songs');
+    if (songsString != null) {
+      final json = jsonDecode(songsString);
+      final songs =
+          json.map<SongModel>((item) => SongModel.fromJson(item)).toList();
+      return Future.value(songs);
+    }
+    return Future.value(<SongModel>[]);
+  }
+
+  saveSongs(List<SongModel>? songs) {
+    final songsString = jsonEncode(songs);
+    dbHelper.insertData('songs', songsString);
   }
 }
--- lib/data/song_repository.dart
+++ lib/data/song_repository.dart
@@ -1,1 +1,34 @@
+import 'package:music_app/domain/song_repository.dart';
 
+import '../domain/song_entity.dart';
+import 'song/local_storage_data.dart';
+import 'song/network_data.dart';
+
+class SongRepositoryImpl implements SongRepository {
+  final SongNetworkData songNetworkData;
+  final SongLocalStorageData songLocalStorageData;
+
+  SongRepositoryImpl({
+    required this.songNetworkData,
+    required this.songLocalStorageData,
+  });
+
+  @override
+  Future<List<SongEntity>> getSongs() async {
+    try {
+      final songs = await songLocalStorageData.getSongs();
+      if (songs != null) {
+        return songs.map((e) => SongEntity.fromSong(e)).toList();
+      } else {
+        final songs = await songNetworkData.getSongs();
+        if (songs != null) {
+          await songLocalStorageData.saveSongs(songs);
+          return songs.map((e) => SongEntity.fromSong(e)).toList();
+        }
+        return [];
+      }
+    } on Exception {
+      rethrow;
+    }
+  }
+}
