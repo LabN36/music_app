fe6b473e9d1281747341d5cf5ff4c11f9be26664
Rishabh Agrawal <rishabh.agrawal@applyboard.com>
2025-03-06 23:09:27 +0530

    feat: Add fromJson method to SongModel and fromSong method to SongEntity for data conversion

--- lib/data/song/song_model.dart
+++ lib/data/song/song_model.dart
@@ -3,4 +3,12 @@ class SongModel {
   final String artistName;
   final List<String> albumImages;
   const SongModel(this.title, this.artistName, this.albumImages);
+
+  static fromJson(item) {
+    return SongModel(
+      item['title'],
+      item['artistName'],
+      item['albumImages'],
+    );
+  }
 }
--- lib/domain/song_entity.dart
+++ lib/domain/song_entity.dart
@@ -1,6 +1,13 @@
+import 'package:music_app/data/song/song_model.dart';
+
 class SongEntity {
   final String title;
   final String artistName;
   final List<String> albumImages;
   const SongEntity(this.title, this.artistName, this.albumImages);
+
+  static SongEntity fromSong(SongModel songModel) {
+    return SongEntity(
+        songModel.title, songModel.artistName, songModel.albumImages);
+  }
 }
